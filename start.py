7ce51c1a4cf06ccfbb07f047b4ec229b318c9507
timoluick <lui17570@spengergasse.at>
2020-10-14 18:46:46 +0200

    Added GAN and visualisation

--- start.py
+++ start.py
@@ -4,22 +4,15 @@ from torch.autograd.variable import Variable
 from torchvision import transforms, datasets
 from torch.utils.data.dataloader import DataLoader
 import matplotlib.pyplot as plt
+from IPython import display
+import numpy as np
 
-
-def mnist_data():
-    compose = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize([.5], [.5])
-        ])
-    out_dir = './dataset'
-    return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
-
-
-# Load data
-data = mnist_data()
-# Create loader with data, so that we can iterate over it
-data_loader = torch.utils.data.DataLoader(data, batch_size=100, shuffle=True)
-# Num batches
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize([0], [1])
+     ])
+data = datasets.MNIST(root='./dataset', train=True, transform=transform, download=True)
+data_loader = torch.utils.data.DataLoader(data, batch_size=128, shuffle=True)
 num_batches = len(data_loader)
 
 
@@ -27,37 +20,17 @@ class DiscriminatorNet(nn.Module):
 
     def __init__(self):
         super(DiscriminatorNet, self).__init__()
-        n_features = 784
-        n_out = 1
 
-        self.hidden0 = nn.Sequential(
-            nn.Linear(n_features, 300),
-            nn.LeakyReLU(.2),
-            nn.Dropout(.3)
-        )
-
-        self.hidden1 = nn.Sequential(
-            nn.Linear(300, 150),
-            nn.LeakyReLU(.2),
-            nn.Dropout(.3)
-        )
-
-        self.hidden2 = nn.Sequential(
-            nn.Linear(150, 50),
-            nn.LeakyReLU(.2),
-            nn.Dropout(.3)
-        )
-
-        self.out = nn.Sequential(
-            nn.Linear(50, n_out),
-            nn.Sigmoid()
-        )
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=2, stride=2)
+        self.linear1 = nn.Linear(392, 20)
+        self.linear2 = nn.Linear(20, 1)
 
     def forward(self, x):
-        x = self.hidden0(x)
-        x = self.hidden1(x)
-        x = self.hidden2(x)
-        x = self.out(x)
+        a = x.shape[0]
+        x = torch.tanh(self.conv1(x))
+        x = x.reshape(a, 1, -1)
+        x = torch.sigmoid(self.linear1(x))
+        x = torch.sigmoid(self.linear2(x))
         return x
 
 
@@ -74,38 +47,22 @@ def vectors_to_images(vectors):
 
 class GeneratorNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_size):
         super(GeneratorNet, self).__init__()
-        n_features = 50
-        n_out = 784
 
-        self.hidden0 = nn.Sequential(
-            nn.Linear(n_features, 128),
-            nn.LeakyReLU(0.2)
-        )
-        self.hidden1 = nn.Sequential(
-            nn.Linear(128, 256),
-            nn.LeakyReLU(0.2)
-        )
-        self.hidden2 = nn.Sequential(
-            nn.Linear(256, 256),
-            nn.LeakyReLU(0.2)
-        )
-
-        self.out = nn.Sequential(
-            nn.Linear(256, n_out),
-            nn.Tanh()
-        )
+        self.linear1 = nn.Linear(in_size, 900)
+        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=2, kernel_size=2)
+        self.conv2 = torch.nn.Conv2d(in_channels=2, out_channels=1, kernel_size=2)
 
     def forward(self, x):
-        x = self.hidden0(x)
-        x = self.hidden1(x)
-        x = self.hidden2(x)
-        x = self.out(x)
+        x = torch.sigmoid(self.linear1(x))
+        x = x.reshape((-1, 1, 30, 30))
+        x = torch.sigmoid(self.conv1(x))
+        x = torch.sigmoid(self.conv2(x))
         return x
 
 
-generator = GeneratorNet()
+generator = GeneratorNet(in_size=50)
 
 
 def noise(size):
@@ -113,19 +70,19 @@ def noise(size):
     return n
 
 
-d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0001)
-g_optimizer = optim.Adam(generator.parameters(), lr=0.0001)
+d_optimizer = optim.Adam(discriminator.parameters(), lr=0.01)
+g_optimizer = optim.Adam(generator.parameters(), lr=0.01)
 
-loss = nn.BCELoss()
+loss = torch.nn.BCELoss()
 
 
 def ones_target(size):
-    data = Variable(torch.ones(size, 1))
+    data = Variable(torch.ones(size, 1, 1))
     return data
 
 
 def zeros_target(size):
-    data = Variable(torch.zeros(size, 1))
+    data = Variable(torch.zeros(size, 1, 1))
     return data
 
 
@@ -168,33 +125,60 @@ def train_generator(optimizer, fake_data):
     return error
 
 
+def plot_durations(x, y, y_net):
+    plt.figure(2)
+    plt.clf()
+    plt.title('Training...')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    lines = plt.plot(x, y, x, y_net)
+    plt.setp(lines[0], linewidth=1, color='r')
+    plt.setp(lines[1], linewidth=1, color='g')
+
+    plt.pause(0.0001)
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+
+
 num_test_samples = 16
 test_noise = noise(num_test_samples)
 
 # Create logger instance
 # Total number of epochs to train
-num_epochs = 2000
-for epoch in range(num_epochs):
-    for n_batch, (real_batch, label_batch) in enumerate(data_loader):
-        N = real_batch.size(0)
-        # 1. Train Discriminator
-        real_data = Variable(images_to_vectors(real_batch))
-        # Generate fake data and detach
-        # (so gradients are not calculated for generator)
-        fake_data = generator(noise(N)).detach()
-        # Train D
-        d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer, real_data, fake_data)
 
-        # 2. Train Generator
-        # Generate fake data
+losses_disc = []
+losses_gen = []
+runs = []
+run = 0
+
+num_epochs = 10000
+for epoch in range(num_epochs):
+    disc_error = []
+    gen_error = []
+    runs.append(epoch)
+    for n_batch, (image_batch, label_batch) in enumerate(data_loader):
+        N = image_batch.size(0)
+        real_data = image_batch
+        fake_data = generator(noise(N)).detach()
+        d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer, real_data, fake_data)
         fake_data = generator(noise(N))
-        # Train G
         g_error = train_generator(g_optimizer, fake_data)
-        # Log batch error
-        #print('Epoch: ' + str(epoch) + ', discriminator error: ' + str(d_error) + ', generator error: ' + str(g_error))
-        # Display Progress every few batches
-        if (n_batch) % 100 == 0:
-            test_images = vectors_to_images(generator(test_noise))
-            test_images = test_images.data
-    plt.imshow(vectors_to_images(generator(noise(1))).detach().numpy()[0, 0])
-    plt.show()
+
+        disc_error.append(d_error.detach().numpy())
+        gen_error.append(g_error.detach().numpy())
+    losses_disc.append(np.mean(disc_error))
+    losses_gen.append(np.mean(gen_error))
+    plot_durations(runs, losses_disc, losses_gen)
+
+    f, axarr = plt.subplots(3, 3)
+    axarr[0, 0].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[0, 1].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[0, 2].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[1, 0].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[1, 1].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[1, 2].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[2, 0].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[2, 1].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    axarr[2, 2].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
+    plt.savefig('a.png')
