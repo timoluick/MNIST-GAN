3967c3a92c37d6e88559aac0b8e5ea8570d4a965
timoluick <lui17570@spengergasse.at>
2020-10-24 22:24:11 +0200

    updated networks

--- start.py
+++ start.py
@@ -11,7 +11,7 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize([0], [1])
      ])
-data = datasets.MNIST(root='./dataset', train=True, transform=transform, download=True)
+data = datasets.MNIST(root='./dataset', train=False, transform=transform, download=True)
 data_loader = torch.utils.data.DataLoader(data, batch_size=128, shuffle=True)
 num_batches = len(data_loader)
 
@@ -21,28 +21,38 @@ class DiscriminatorNet(nn.Module):
     def __init__(self):
         super(DiscriminatorNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=2, stride=2)
-        self.linear1 = nn.Linear(392, 20)
-        self.linear2 = nn.Linear(20, 1)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.2)
+        )
+        self.linear1 = nn.Sequential(
+            nn.Linear(676, 20),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.2)
+        )
+        self.linear2 = nn.Sequential(
+            nn.Linear(20, 1),
+            nn.Sigmoid()
+        )
+
 
     def forward(self, x):
-        a = x.shape[0]
-        x = torch.tanh(self.conv1(x))
-        x = x.reshape(a, 1, -1)
-        x = torch.sigmoid(self.linear1(x))
-        x = torch.sigmoid(self.linear2(x))
+        n = x.size(0)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.reshape(n, 1, -1)
+        x = self.linear1(x)
+        x = self.linear2(x)
         return x
 
 
 discriminator = DiscriminatorNet()
-
-
-def images_to_vectors(images):
-    return images.view(images.size(0), 784)
-
-
-def vectors_to_images(vectors):
-    return vectors.view(vectors.size(0), 1, 28, 28)
 
 
 class GeneratorNet(nn.Module):
@@ -50,28 +60,41 @@ class GeneratorNet(nn.Module):
     def __init__(self, in_size):
         super(GeneratorNet, self).__init__()
 
-        self.linear1 = nn.Linear(in_size, 900)
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=2, kernel_size=2)
-        self.conv2 = torch.nn.Conv2d(in_channels=2, out_channels=1, kernel_size=2)
+        self.linear1 = nn.Sequential(
+            nn.Linear(in_size, 200),
+            nn.LeakyReLU(0.3)
+        )
+        self.linear2 = nn.Sequential(
+            nn.Linear(200, 300),
+            nn.LeakyReLU(0.3)
+        )
+        self.linear3 = nn.Sequential(
+            nn.Linear(300, 784),
+            nn.LeakyReLU(0.3)
+        )
+        self.linear4 = nn.Sequential(
+            nn.Linear(500, 784),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        x = torch.sigmoid(self.linear1(x))
-        x = x.reshape((-1, 1, 30, 30))
-        x = torch.sigmoid(self.conv1(x))
-        x = torch.sigmoid(self.conv2(x))
+        x = self.linear1(x)
+        x = self.linear2(x)
+        x = self.linear3(x)
+        #x = self.linear4(x)
         return x
 
 
-generator = GeneratorNet(in_size=50)
+generator = GeneratorNet(in_size=100)
 
 
 def noise(size):
-    n = Variable(torch.randn(size, 50))
+    n = Variable(torch.randn(size, 100))
     return n
 
 
 d_optimizer = optim.Adam(discriminator.parameters(), lr=0.001)
-g_optimizer = optim.Adam(generator.parameters(), lr=0.001)
+g_optimizer = optim.Adam(generator.parameters(), lr=0.0002)
 
 loss = torch.nn.BCELoss()
 
@@ -88,40 +111,32 @@ def zeros_target(size):
 
 def train_discriminator(optimizer, real_data, fake_data):
     N = real_data.size(0)
-    # Reset gradients
     optimizer.zero_grad()
-
-    # 1.1 Train on Real Data
     prediction_real = discriminator(real_data)
-    # Calculate error and backpropagate
     error_real = loss(prediction_real, ones_target(N))
     error_real.backward()
-
-    # 1.2 Train on Fake Data
     prediction_fake = discriminator(fake_data)
-    # Calculate error and backpropagate
     error_fake = loss(prediction_fake, zeros_target(N))
     error_fake.backward()
-
-    # 1.3 Update weights with gradients
     optimizer.step()
-
-    # Return error and predictions for real and fake inputs
     return error_real + error_fake, prediction_real, prediction_fake
+
+
+def to_img(x):
+    return x.reshape(x.size(0), 1, 28, 28)
+
+
+def to_vec(x):
+    return x.reshape(x.size(0), 784)
 
 
 def train_generator(optimizer, fake_data):
     N = fake_data.size(0)
-    # Reset gradients
     optimizer.zero_grad()
-    # Sample noise and generate fake data
     prediction = discriminator(fake_data)
-    # Calculate error and backpropagate
     error = loss(prediction, ones_target(N))
     error.backward()
-    # Update weights with gradients
     optimizer.step()
-    # Return error
     return error
 
 
@@ -133,24 +148,20 @@ def plot_durations(x, y, y_net):
     plt.ylabel('y')
 
     lines = plt.plot(x, y, x, y_net)
-    plt.setp(lines[0], linewidth=1, color='g')
-    plt.setp(lines[1], linewidth=1, color='r')
+    plt.setp(lines[0], linewidth=1, color='g', label='discriminator')
+    plt.setp(lines[1], linewidth=1, color='r', label='generator')
 
     plt.pause(0.0001)
     display.clear_output(wait=True)
     display.display(plt.gcf())
 
 
-num_test_samples = 16
-test_noise = noise(num_test_samples)
-
-# Create logger instance
-# Total number of epochs to train
-
 losses_disc = []
 losses_gen = []
 runs = []
 run = 0
+
+test_noise = noise(9)
 
 num_epochs = 10000
 for epoch in range(num_epochs):
@@ -160,27 +171,41 @@ for epoch in range(num_epochs):
     for n_batch, (image_batch, label_batch) in enumerate(data_loader):
         N = image_batch.size(0)
         real_data = image_batch
-        fake_data = generator(noise(N)).detach()
+
+        fake_data = to_img(generator(noise(N)).detach())
+
         d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer, real_data, fake_data)
-        fake_data = generator(noise(N))
+
+        fake_data = to_img(generator(noise(N)))
         g_error = train_generator(g_optimizer, fake_data)
 
         disc_error.append(d_error.detach().numpy())
         gen_error.append(g_error.detach().numpy())
     losses_disc.append(np.mean(disc_error))
     losses_gen.append(np.mean(gen_error))
-    #plot_durations(runs, losses_disc, losses_gen)
+
+    plot_durations(runs, losses_disc, losses_gen)
 
     print('Epoch: ' + str(epoch))
 
     f, axarr = plt.subplots(3, 3)
-    axarr[0, 0].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[0, 1].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[0, 2].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[1, 0].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[1, 1].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[1, 2].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[2, 0].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[2, 1].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    axarr[2, 2].imshow(generator(noise(1)).detach().numpy()[0, 0], cmap='gray')
-    plt.savefig('a.png')
+    test_images = to_img(generator(test_noise)).detach().numpy()
+    '''axarr[0, 0].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[0, 1].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[0, 2].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[1, 0].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[1, 1].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[1, 2].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[2, 0].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[2, 1].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')
+axarr[2, 2].imshow(to_img(generator(noise(1))).detach().numpy()[0, 0], cmap='gray')'''
+    axarr[0, 0].imshow(test_images[0, 0], cmap='gray')
+    axarr[0, 1].imshow(test_images[1, 0], cmap='gray')
+    axarr[0, 2].imshow(test_images[2, 0], cmap='gray')
+    axarr[1, 0].imshow(test_images[3, 0], cmap='gray')
+    axarr[1, 1].imshow(test_images[4, 0], cmap='gray')
+    axarr[1, 2].imshow(test_images[5, 0], cmap='gray')
+    axarr[2, 0].imshow(test_images[6, 0], cmap='gray')
+    axarr[2, 1].imshow(test_images[7, 0], cmap='gray')
+    axarr[2, 2].imshow(test_images[8, 0], cmap='gray')
+    plt.savefig(str(epoch) + '.png')
